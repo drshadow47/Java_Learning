8ad6a8976d7856b980410cd8595347ad39848178
drshadow <sanjeev@biocipher.in>
2023-09-19 22:01:16 +0530

    test

---
+++ src/main/java/com/example/demo/literals/Literals.java
@@ -0,0 +1,40 @@
+package com.example.demo.literals;
+
+import com.example.demo.DemoApplication;
+import org.springframework.boot.SpringApplication;
+
+public class Literals {
+    /**
+     *  DESCRIPTION :::
+     * Literals in Java:
+     * In Java, a literal represents a constant value that can be directly assigned to a variable. Literals can be of various types, including numeric literals, character literals, string literals, boolean literals, and null literals. Here's a breakdown of different types of literals in Java:
+     * <p>
+     * Numeric Literals: Numeric literals are used to represent numbers. They can be further categorized into:
+     * <p>
+     * Integer Literals: These are whole numbers without decimal points, such as 42 or -10.
+     * Floating-Point Literals: These are numbers with decimal points, like 3.14 or -0.5.
+     * Hexadecimal Literals: Represented with a 0x or 0X prefix, like 0xFF (255 in decimal).
+     * Binary Literals: Represented with a 0b or 0B prefix, like 0b1010 (10 in decimal).
+     * Character Literals: These represent individual characters enclosed in single quotes, such as 'A' or '7'.
+     * <p>
+     * String Literals: String literals are sequences of characters enclosed in double quotes, like "Hello, World!".
+     * <p>
+     * Boolean Literals: Java has two boolean literals: true and false, representing the boolean values.
+     * <p>
+     * Null Literal: The null literal represents a reference with no value assigned.
+     * <p>
+     * Here's an example of using literals in Java:
+     *
+     * int age = 25;          // Integer literal
+     * double pi = 3.14159;   // Floating-point literal
+     * char grade = 'A';      // Character literal
+     * String message = "Hello, World!"; // String literal
+     * boolean isJavaFun = true; // Boolean literal
+     * Object obj = null;     // Null literal
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
